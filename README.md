2ab6fc954bde0e6158c32d48b479f6b3f9041fed
mark279455 <mark279455@ntlworld.com>
2023-11-08 13:29:05 +0000

    competed header & css

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# oasis_pp5
+# Code Institute
+
+Welcome Chris Z.,
+
+We have preinstalled all of the tools you need to get started.
+
+Happy coding!
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['SECRET_KEY'] = "django-insecure-lieb_@^h(asntzl!o6or0d4_s@l9sc3)z)%$ehn@kdh8)d37j="
+os.environ['DEVELOPMENT'] = "true"
